976d2f95fc2d0e2c79c624bb3b230497f1987de2
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-12-10 23:26:29 -0300

    feat(entity): adicionar entidade de usuário

---
+++ src/entities/User/User.ts
@@ -0,0 +1,33 @@
+import IUser from "./IUser";
+
+class User implements IUser {
+	private _id: string;
+	private _name: string;
+	private _email: string;
+	private _password: string;
+
+	constructor(name: string, email: string, password: string) {
+		this._id = "aa";
+		this._name = name;
+		this._email = email;
+		this._password = password;
+	}
+
+	get id() {
+		return this._id;
+	}
+
+	get name() {
+		return this._name;
+	}
+
+	get email() {
+		return this._email;
+	}
+
+	get password() {
+		return this._password;
+	}
+}
+
+export default User;
