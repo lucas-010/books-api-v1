77ae3232bce66e96791cf0a3e247b4b431a81da2
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-12-12 12:50:39 -0300

    chore(db): adicionar migration para tabela de livros

---
+++ src/database/migrations/0002_create_book.ts
@@ -0,0 +1,23 @@
+import { Knex } from "knex";
+
+export const up = async (knex: Knex) => {
+	await knex.schema
+		.createTable("book", (table) => {
+			table.string("id").primary().index();
+			table.string("title").notNullable().index().checkLength(">=", 2);
+			table.integer("pages").notNullable().checkLength(">=", 1);
+			table.string("imgUrl", 255);
+			table.text("description").notNullable().checkLength(">=", 10);
+			table.integer("author_id").unsigned();
+			table.foreign("author_id").references("author.id");
+		})
+		.then(() => {
+			console.log("# Created table book");
+		});
+};
+
+export const down = async (knex: Knex) => {
+	await knex.schema.dropTable("book").then(() => {
+		console.log("# Dropped table book");
+	});
+};
