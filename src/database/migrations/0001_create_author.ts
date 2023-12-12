c5a5125dff3c79ec275660ea370ad5b93002957c
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-12-12 12:56:19 -0300

    chore(db): modificando id da migration autor de string para uuid

--- src/database/migrations/0001_create_author.ts
+++ src/database/migrations/0001_create_author.ts
@@ -3,7 +3,7 @@ import { Knex } from "knex";
 export const up = async (knex: Knex) => {
 	await knex.schema
 		.createTable("author", (table) => {
-			table.string("id").primary().index();
+			table.uuid("id").primary().index();
 			table.string("name").notNullable().unique().index().checkLength(">=", 2);
 		})
 		.then(() => {
