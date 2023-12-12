f1165ee29eb5f77dd93240750ceb2b8c73dc1102
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-12-12 20:03:47 -0300

    feat(repository): modificação no nome da classe de repositório para autores

--- src/repositories/AuthorRepository.ts
+++ src/repositories/AuthorRepository.ts
@@ -1,10 +1,10 @@
 import Author from "../entities/Author/Author";
 import BaseRepository from "./BaseRepository";
 
-class UserRepository extends BaseRepository<Author> {
+class AuthorRepository extends BaseRepository<Author> {
 	constructor() {
 		super("author");
 	}
 }
 
-export default UserRepository;
+export default AuthorRepository;
