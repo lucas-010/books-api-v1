12a41c7d85564003d5efb74995fc4d338dc4e68d
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-12-12 19:57:17 -0300

    feat(repository): repositório base que servirá efetuar ações no banco de dados

---
+++ src/repositories/BaseRepository.ts
@@ -0,0 +1,43 @@
+import { Knex } from "knex";
+import { database } from "../database";
+
+abstract class BaseRepository<T> {
+	protected readonly tableName: string;
+	protected database: Knex;
+
+	constructor(tableName: string) {
+		this.tableName = tableName;
+		this.database = database;
+	}
+
+	async selectAll(page: number = 1, limit: number = 10): Promise<T[]> {
+		const offset = (page - 1) & limit;
+		return await this.database(this.tableName)
+			.select()
+			.offset(offset)
+			.limit(limit);
+	}
+
+	async selectById(id: string): Promise<T> {
+		return await this.database(this.tableName).select().where("id", id).first();
+	}
+
+	async insert(item: T): Promise<string> {
+		return (await this.database(this.tableName).insert(item).returning("id"))[0]
+			.id;
+	}
+
+	async delete(id: string): Promise<boolean> {
+		return (await this.database(this.tableName).where("id", id).del()) > 0;
+	}
+
+	async update(item: Partial<T>, id: string): Promise<boolean> {
+		const result = await this.database(this.tableName)
+			.where("id", id)
+			.update(item);
+
+		return result > 0;
+	}
+}
+
+export default BaseRepository;
