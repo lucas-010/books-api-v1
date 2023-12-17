1edc12022aeee8372ecec8df628be0aa8567729d
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-12-17 15:28:21 -0300

    feat(repository): implementação do método para mapear objeto livro

--- src/repositories/BookRepository.ts
+++ src/repositories/BookRepository.ts
@@ -5,6 +5,17 @@ class BookRepository extends BaseRepository<Book> {
 	constructor() {
 		super("book");
 	}
+
+	mapToDatabaseObject(item: Book): Record<string, any> {
+		return {
+			id: item.id,
+			title: item.title,
+			pages: item.pages,
+			description: item.description,
+			imgUrl: item.imgUrl,
+			authorId: item.authorId,
+		};
+	}
 }
 
 export default BookRepository;
