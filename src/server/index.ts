180b09eade6a4e6dd334bc0145fb548c890e88f7
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-12-17 23:18:30 -0300

    feat(server): adicionar rotas para livros e usuários

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,15 @@
 import express from "express";
+import authorRouter from "./routes/author.routes";
+import bookRouter from "./routes/book.routes";
+import userRouter from "./routes/user.routes";
 
 const server = express();
 
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
+
+server.use("/author", authorRouter);
+server.use("/book", bookRouter);
+server.use("/user", userRouter);
 
 export default server;
