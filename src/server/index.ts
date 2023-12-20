e62a7f2c15e70d35af8b7b88ff03e8b62baca30a
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-12-20 18:56:38 -0300

    feat(router): utilizando o router para login

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import authorRouter from "./routes/author.routes";
 import bookRouter from "./routes/book.routes";
 import userRouter from "./routes/user.routes";
+import loginRouter from "./routes/login.routes";
 
 const server = express();
 
@@ -11,5 +12,6 @@ server.use(express.json());
 server.use("/author", authorRouter);
 server.use("/book", bookRouter);
 server.use("/user", userRouter);
+server.use("/login", loginRouter);
 
 export default server;
