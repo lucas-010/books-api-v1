98700c727aaed399b4bf2d54a9daaf232d7571de
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-12-20 13:55:27 -0300

    feat(route): adicionar as validações necessárias nas rotas

--- src/server/routes/user.routes.ts
+++ src/server/routes/user.routes.ts
@@ -4,13 +4,17 @@ import getAllUser from "../handlers/user/getAll";
 import getByIdUser from "../handlers/user/getById";
 import updateUser from "../handlers/user/update";
 import deleteUser from "../handlers/user/delete";
+import validationUser from "../validations/validationUser";
+import validate from "../middlewares/validate";
+import validationPaginationOptions from "../validations/validationPaginationOptions";
+import validationId from "../validations/validationId";
 
 const userRouter = Router();
 
-userRouter.post("/", createUser);
-userRouter.get("/", getAllUser);
-userRouter.get("/:id", getByIdUser);
-userRouter.put("/:id", updateUser);
-userRouter.delete("/:id", deleteUser);
+userRouter.post("/", validationUser, validate, createUser);
+userRouter.get("/", validationPaginationOptions, validate, getAllUser);
+userRouter.get("/:id", validationId, validate, getByIdUser);
+userRouter.put("/:id", validationId, validate, updateUser);
+userRouter.delete("/:id", validationId, validate, deleteUser);
 
 export default userRouter;
