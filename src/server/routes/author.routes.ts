0fcda0b57c8ef2d607175ec8a107d28aa09dad39
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-12-20 13:55:40 -0300

    feat(route): adicionar as validações necessárias nas rotas

--- src/server/routes/author.routes.ts
+++ src/server/routes/author.routes.ts
@@ -4,13 +4,17 @@ import getByIdAuthor from "../handlers/author/getById";
 import getAllAuthor from "../handlers/author/getAll";
 import updateAuthor from "../handlers/author/update";
 import deleteAuthor from "../handlers/author/delete";
+import validationAuthor from "../validations/validationAuthor";
+import validate from "../middlewares/validate";
+import validationId from "../validations/validationId";
+import validationPaginationOptions from "../validations/validationPaginationOptions";
 
 const authorRouter = Router();
 
-authorRouter.post("/", createAuthor);
-authorRouter.get("/:id", getByIdAuthor);
-authorRouter.get("/", getAllAuthor);
-authorRouter.put("/:id", updateAuthor);
-authorRouter.delete("/:id", deleteAuthor);
+authorRouter.post("/", validationAuthor, validate, createAuthor);
+authorRouter.get("/:id", validationId, validate, getByIdAuthor);
+authorRouter.get("/", validationPaginationOptions, validate, getAllAuthor);
+authorRouter.put("/:id", validationId, validate, updateAuthor);
+authorRouter.delete("/:id", validationId, validate, deleteAuthor);
 
 export default authorRouter;
