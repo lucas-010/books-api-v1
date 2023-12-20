e2ff1ae0006c3ec430e48e06eaa811700a5e7219
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-12-20 13:52:02 -0300

    feat(validation): objeto request da função validate com tipos genéricos

--- src/server/middlewares/validate.ts
+++ src/server/middlewares/validate.ts
@@ -2,7 +2,11 @@ import { NextFunction, Request, Response } from "express";
 import { validationResult } from "express-validator";
 import { StatusCodes } from "http-status-codes";
 
-function validate(req: Request, res: Response, next: NextFunction) {
+function validate(
+	req: Request<any, any, any, any>,
+	res: Response,
+	next: NextFunction,
+) {
 	const errors = validationResult(req);
 	if (!errors.isEmpty())
 		return res.status(StatusCodes.BAD_REQUEST).json({ errors: errors.array() });
